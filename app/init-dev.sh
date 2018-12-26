3ea8097273067032f4a2c448533d5f1d09751221
Rahadian Bayu Permadi <bayu@Rahadians-MacBook-Pro.local>
2018-12-26 16:05:33 +0700

    add command to delete sqlitedb and to setup database

--- app/init-dev.sh
+++ app/init-dev.sh
@@ -1,1 +1,5 @@
+cd flask_app
+rm database.db
+python database_setup.py
+
 flask run --host=0.0.0.0 --port=8080
--- app/init-prod.sh
+++ app/init-prod.sh
@@ -2,5 +2,11 @@
 
 set -e
 
-cd /app
+cd /app/flask_app
+rm database.db
+python database_setup.py
+
+cd ../
+
+python setup-db.sh
 waitress-serve --call 'flask_app:create_app'
